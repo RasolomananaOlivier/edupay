be7b4105e5812785397abc28fd27d67b276dc1e0
Rasolomanana Olivier <herimanitraolivier@gmail.com>
2024-06-17 06:40:11 +0300

    feat: add support for listing, creatin and deleting amounts

---
+++ src/main/java/repository/amount/AmountRepositoryImpl.java
@@ -0,0 +1,120 @@
+package repository.amount;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import database.Database;
+import model.MonthAmount;
+
+public class AmountRepositoryImpl implements AmountRepository {
+
+	@Override
+	public List<MonthAmount> getMonthAmounts() {
+		try (Connection connection = Database.getConnection()) {
+
+			String sql = "SELECT * FROM \"MonthAmount\" "
+					+ "INNER JOIN \"Level\" ON \"Level\".\"id\" = \"MonthAmount\".\"levelId\"";
+
+			Statement st = connection.createStatement();
+
+			ResultSet result = st.executeQuery(sql);
+
+			List<MonthAmount> amounts = new ArrayList<MonthAmount>();
+
+			while (result.next()) {
+				amounts.add(MonthAmount.fromResultSet(result));
+			}
+
+			return amounts;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	@Override
+	public MonthAmount getMonthAmount(int id) {
+		try (Connection connection = Database.getConnection()) {
+
+			String sql = "SELECT * FROM \"MonthAmount\" "
+					+ "INNER JOIN \"Level\" ON \"Level\".\"id\" = \"MonthAmount\".\"levelId\" "
+					+ "WHERE \"MonthAmount\".\"id\" = ? ";
+
+			PreparedStatement st = connection.prepareStatement(sql);
+
+			st.setInt(1, id);
+
+			ResultSet result = st.executeQuery(sql);
+
+			MonthAmount amount = null;
+
+			while (result.next()) {
+				amount = MonthAmount.fromResultSet(result);
+			}
+
+			return amount;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	@Override
+	public void addLMonthAmount(MonthAmount amount) {
+		try (Connection connection = Database.getConnection()) {
+
+			String sql = "INSERT INTO \"MonthAmount\" "
+					+ "(\"value\", \"levelId\", \"createdAt\", \"updatedAt\") "
+					+ "VALUES (?, ?, ?, ?)";
+
+			PreparedStatement st = connection.prepareStatement(sql);
+
+			//st.setInt(1, amount.getId());
+			st.setInt(1, amount.getValue());
+			st.setInt(2, amount.getLevelId());
+			st.setDate(3, new Date(amount.getCreatedAt().getTime()));
+			st.setDate(4, new Date(amount.getUpdatedAt().getTime()));
+
+			st.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void updateMonthAmount(MonthAmount amount) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteMonthAmount(int id) {
+		try (Connection connection = Database.getConnection()) {
+
+			String sql = "DELETE FROM \"MonthAmount\" "
+					+ "WHERE \"id\" = ?";
+
+			PreparedStatement st = connection.prepareStatement(sql);
+
+			st.setInt(1, id);
+
+			st.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
